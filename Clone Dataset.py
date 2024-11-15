4f8b33d1491f438507f0597dac1f7819f2208585
Biuuu2309 <minhvu7a1@gmail.com>
2024-11-15 20:25:04 +0700

    aaa

---
+++ Clone Dataset.py
@@ -0,0 +1,14 @@
+import kagglehub
+import csv
+
+# Download latest version
+# path = kagglehub.dataset_download("deepcontractor/squid-game-netflix-twitter-data")
+# print("Path to dataset files:", path)
+
+# # Download latest version
+# path = kagglehub.dataset_download("stephenofarrell/league-of-legends-european-championship-2019")
+# print("Path to dataset files:", path)
+
+# Download latest version
+path = kagglehub.dataset_download("artermiloff/steam-games-dataset")
+print("Path to dataset files:", path)
