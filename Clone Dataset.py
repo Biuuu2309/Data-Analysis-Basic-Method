542700641d72cf41279b4003d3d574e88fb6b31e
Biuuu2309 <minhvu7a1@gmail.com>
2024-11-17 20:01:16 +0700

    aaa

--- Clone Dataset.py
+++ Clone Dataset.py
@@ -1,14 +1,20 @@
-import kagglehub
-import csv
-
-# Download latest version
-# path = kagglehub.dataset_download("deepcontractor/squid-game-netflix-twitter-data")
-# print("Path to dataset files:", path)
+# import kagglehub
+# import csv
 
 # # Download latest version
-# path = kagglehub.dataset_download("stephenofarrell/league-of-legends-european-championship-2019")
+# # path = kagglehub.dataset_download("deepcontractor/squid-game-netflix-twitter-data")
+# # print("Path to dataset files:", path)
+
+# # # Download latest version
+# # path = kagglehub.dataset_download("stephenofarrell/league-of-legends-european-championship-2019")
+# # print("Path to dataset files:", path)
+
+# # Download latest version
+# path = kagglehub.dataset_download("artermiloff/steam-games-dataset")
 # print("Path to dataset files:", path)
+import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("artermiloff/steam-games-dataset")
+path = kagglehub.dataset_download("junhachoi/all-ranked-solo-games-on-kr-server-24-hours")
+
 print("Path to dataset files:", path)
---
+++ SQLQuery_LOL.sql
@@ -0,0 +1,1 @@
+SELECT * FROM LOL_Data
